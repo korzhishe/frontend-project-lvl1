8d359be0c70bcc2d3d97011a949f8dfd3597359c
akozhemyakin <agkozhemyakin@gmail.com>
2021-09-21 22:07:44 +0300

    added brain-calc game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import readlineSync from "readline-sync";
+import {make} from "../src/index.js";
+import {cons} from "@hexlet/pairs";
+
+const WELCOME = 'Welcome to the Brain Calc!';
+const QUESTION_NAME = `May I have your name? `;
+const QUESTIONS = [
+    cons(`4 + 10`, () => '14'),
+    cons(`25 - 11`, () => '14'),
+    cons(`25 * 7`, () => '175'),
+];
+
+console.log(WELCOME);
+const name = readlineSync.question(QUESTION_NAME);
+const game = make(QUESTIONS);
+const isWin = game(name, `AWhat is the result of the expression?.`);
+if (isWin) console.log(`Congratulations, ${name}!`)
