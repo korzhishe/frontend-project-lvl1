220f13a6d85720451c00078d8590f2cdb26279fe
akozhemyakin <agkozhemyakin@gmail.com>
2021-09-22 22:57:42 +0300

    changed brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -3,7 +3,7 @@ import readlineSync from "readline-sync";
 import {make_v2} from "../src/index.js";
 import {cons} from "@hexlet/pairs";
 
-const WELCOME = 'Welcome to the Brain Calc!';
+const WELCOME = 'Welcome to the Brain Gcd!';
 const QUESTION_NAME = `May I have your name? `;
 const QUESTIONS = [
     cons(`25 50`, () => '25'),
