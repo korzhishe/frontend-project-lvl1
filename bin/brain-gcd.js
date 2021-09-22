969e59f826190526eb773f163cd8263f8d5babf3
akozhemyakin <agkozhemyakin@gmail.com>
2021-09-22 22:46:55 +0300

    added brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+import {make_v2} from "../src/index.js";
+import {cons} from "@hexlet/pairs";
+
+const WELCOME = 'Welcome to the Brain Calc!';
+const QUESTION_NAME = `May I have your name? `;
+const QUESTIONS = [
+    cons(`25 50`, () => '25'),
+    cons(`100 52`, () => '4'),
+    cons(`3 9`, () => '3'),
+];
+
+console.log(WELCOME);
+const name = readlineSync.question(QUESTION_NAME);
+const unCorrectAnswer = (answer, correct, name) => `'${answer}' is wrong answer ;(. Correct answer was '${correct}'.
+                Let's try again, ${name}!`;
+const game = make_v2(QUESTIONS, unCorrectAnswer);
+const isWin = game(name, `Find the greatest common divisor of given numbers.`);
+if (isWin) console.log(`Congratulations, ${name}!`)
--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ export const make = (questions) => {
         }, true);
     }
 }
+
+export const make_v2 = (questions, unCorrectAnswer) => {
+    return (name, task) => {
+        console.log(`Hello, ${name}!`);
+        console.log(task);
+        return questions.reduce((prev, cur) => {
+            console.log(`Question: ${car(cur)}`);
+            const answer = readlineSync.question(`Your answer: `);
+            if (answer === cdr(cur)()) console.log(`Correct!`);
+            else console.log(unCorrectAnswer(answer, cdr(cur)(), name));
+            return (answer === cdr(cur)()) && prev;
+        }, true);
+    }
+}
