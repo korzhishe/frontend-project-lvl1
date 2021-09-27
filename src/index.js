ac492574b9fe4f380f4c19230a347e61fadff053
akozhemyakin <agkozhemyakin@gmail.com>
2021-09-27 23:02:26 +0300

    refactored engine

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import readlineSync from "readline-sync";
 import {car, cdr} from "@hexlet/pairs";
 
-export const make = (questions) => {
-    return (name, task) => {
+const WELCOME = 'Welcome to the Brain Games!';
+const QUESTION_NAME = `May I have your name? `;
+
+export const make = () => {
+    console.log(WELCOME);
+    const name = readlineSync.question(QUESTION_NAME);
+    return (questions, task, unCorrectAnswer) => {
         console.log(`Hello, ${name}!`);
         console.log(task);
-        return questions.reduce((prev, cur) => {
+        const isWin = questions.reduce((prev, cur) => {
+            if (!prev) return false;
             console.log(`Question: ${car(cur)}`);
             const answer = readlineSync.question(`Your answer: `);
-            if (answer === cdr(cur)()) console.log(`Correct!`);
-            return (answer === cdr(cur)()) && prev;
+            if (answer === cdr(cur)()) {
+                console.log(`Correct!`);
+                return true;
+            }
+            else {
+                if(unCorrectAnswer != undefined) console.log(unCorrectAnswer(answer, cdr(cur)(), name));
+                return false;
+            }
         }, true);
+        if (isWin) console.log(`Congratulations, ${name}!`);
+        return isWin;
     }
 }
 
