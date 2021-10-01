417389ab962f066264f780b7fa95a616121310b6
akozhemyakin <agkozhemyakin@gmail.com>
2021-10-01 10:17:54 +0300

    refactored brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -8,12 +8,11 @@ const gcd = (a, b) => {
   return gcd(b, a % b);
 };
 
-const QUESTIONS = [];
-for (let i = 0; i < 3; i += 1) {
+const getQuestion = () => {
   const x = _.random(0, 100);
   const y = _.random(0, 100);
-  QUESTIONS.push(cons(`${x} ${y}`, () => `${gcd(x, y)}`));
-}
+  return cons(`${x} ${y}`, () => `${gcd(x, y)}`);
+};
 
-const game = make();
-game(QUESTIONS, 'Find the greatest common divisor of given numbers.');
+const game = make(getQuestion);
+game('Find the greatest common divisor of given numbers.');
