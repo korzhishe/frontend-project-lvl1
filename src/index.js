2d7da9607aab98303b1a7621bc1c3a4e8277862b
akozhemyakin <agkozhemyakin@gmail.com>
2021-10-01 10:06:05 +0300

    refactored make game

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import readlineSync from 'readline-sync';
-import { car, cdr } from '@hexlet/pairs';
+import {car, cdr, cons} from '@hexlet/pairs';
 
-const WELCOME = 'Welcome to the Brain Games!';
-const QUESTION_NAME = 'May I have your name? ';
+const welcomeToTheBrainGames = 'Welcome to the Brain Games!';
+const questionName = 'May I have your name? ';
 const unCorrectAnswer = (answer, correct, name) => `'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${name}!`;
-const YOUR_ANSWER = 'Your answer: ';
+const yourAnswer = 'Your answer: ';
+const numberQuestions = 3;
 
-export default () => {
-  console.log(WELCOME);
-  const name = readlineSync.question(QUESTION_NAME);
+export default (getQuestion) => {
+  console.log(welcomeToTheBrainGames);
+  const name = readlineSync.question(questionName);
   console.log(`Hello, ${name}!`);
-  return (questions, task) => {
+
+  const questions = [...Array(numberQuestions).keys()].map(getQuestion);
+
+  return (task) => {
     console.log(task);
     const isWin = questions.reduce((prev, cur) => {
       if (!prev) return false;
       console.log(`Question: ${car(cur)}`);
-      const answer = readlineSync.question(YOUR_ANSWER);
+      const answer = readlineSync.question(yourAnswer);
       if (answer === cdr(cur)()) {
         console.log('Correct!');
         return true;
