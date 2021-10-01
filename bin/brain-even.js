8357bfb20633d06b55680a61b919d5dfd3ac9526
akozhemyakin <agkozhemyakin@gmail.com>
2021-10-01 10:09:44 +0300

    refactored brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,11 +3,10 @@ import _ from 'lodash';
 import { cons } from '@hexlet/pairs';
 import make from '../src/index.js';
 
-const QUESTIONS = [];
-for (let i = 0; i < 3; i += 1) {
+const getQuestion = () => {
   const x = _.random(0, 100);
-  QUESTIONS.push(cons(`${x}`, () => (x % 2 === 0 ? 'yes' : 'no')));
-}
+  return cons(`${x}`, () => (x % 2 === 0 ? 'yes' : 'no'));
+};
 
-const game = make();
-game(QUESTIONS, 'Answer "yes" if the number is even, otherwise answer "no".');
+const game = make(getQuestion);
+game('Answer "yes" if the number is even, otherwise answer "no".');
