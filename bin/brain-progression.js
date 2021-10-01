02c1491b4f3b6a03e67c7e8357251cdebfae0e37
akozhemyakin <agkozhemyakin@gmail.com>
2021-10-01 10:48:57 +0300

    refactored brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -3,20 +3,15 @@ import _ from 'lodash';
 import { cons } from '@hexlet/pairs';
 import make from '../src/index.js';
 
-const QUESTIONS = [];
-for (let i = 0; i < 3; i += 1) {
-  const pr = _.random(0, 10);
+const getQuestion = () => {
+  const pr = _.random(1, 10);
   const index = _.random(0, 9);
-  let curNumber = _.random(0, 100);
-  const mas = [];
-  for (let j = 0; j < 10; j += 1) {
-    mas.push(curNumber);
-    curNumber += pr;
-  }
+  const startNumber = _.random(0, 100);
+  const mas = _.range(startNumber, startNumber + (pr * 10), pr);
   const answer = mas[index];
   mas[index] = '..';
-  QUESTIONS.push(cons(`${mas.join(' ')}`, () => `${answer}`));
-}
+  return cons(`${mas.join(' ')}`, () => `${answer}`);
+};
 
-const game = make();
-game(QUESTIONS, 'What number is missing in the progression?');
+const game = make(getQuestion);
+game('What number is missing in the progression?');
